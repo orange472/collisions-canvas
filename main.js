bfc4737c83db30ea6ae404f521492e8f6c3e6854
Tony Li <55900779+orange472@users.noreply.github.com>
2021-06-05 02:25:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const startButton = document.getElementById("startButton");
+const numCirclesSlider = document.getElementById("numCirclesSlider");
 
 let circles = [];
 
@@ -25,6 +26,10 @@ class Circle {
         ctx.closePath();
     }
 
+    getIndex() {
+        return this.index;
+    }
+
     updateVelocity(m1, m2, v1, v2) {
         var v = 0;
 
@@ -46,7 +51,7 @@ class Circle {
     }
 
     updatePosition() {
-        if(this.xpos + this.radius + this.xvel > canvas.width || this.xpos - this.radius + this.xvel< 0) {
+        if(this.xpos + this.radius + this.xvel >= canvas.width || this.xpos - this.radius + this.xvel < 0) {
             this.xvel = -this.xvel;
         }
         if(this.ypos + this.radius + this.yvel > canvas.height || this.ypos - this.radius + this.yvel < 0) {
@@ -59,31 +64,16 @@ class Circle {
                     var tempX = this.xvel;
                     var tempY = this.yvel;
 
-                    this.color = "blue";
                     this.xvel = this.updateVelocity(this.mass, circles[j].mass, this.xvel, circles[j].xvel);
                     this.yvel = this.updateVelocity(this.mass, circles[j].mass, this.yvel, circles[j].yvel);
                     circles[j].xvel = ((this.mass * tempX + circles[j].mass * circles[j].xvel) - (this.mass * this.xvel)) / circles[j].mass;
                     circles[j].yvel = ((this.mass * tempY + circles[j].mass * circles[j].yvel) - (this.mass * this.yvel)) / circles[j].mass;
                 }
                 else {
-                    this.color = "black";
+
                 }
             }
         }
-
-        //let xPosition = this.xpos + this.xvel + this.radius;
-        //let yPosition = this.ypos + this.yvel + this.radius;
-
-        /**if(this.xpos + this.xvel + this.radius > canvas.width) {
-            this.xpos += canvas.width - this.xpos - this.radius;
-        }
-        if(this.ypos + this.yvel + this.radius > canvas.height) {
-            this.ypos += canvas.height - this.ypos - this.radius;
-        }
-        if(!(this.xpos + this.xvel + this.radius > canvas.width || this.ypos + this.yvel + this.radius > canvas.height)) {
-            this.xpos += this.xvel;
-            this.ypos += this.yvel;
-        }**/
 
         this.xpos += this.xvel;
         this.ypos += this.yvel;
@@ -92,33 +82,43 @@ class Circle {
     }
 }
 
-
 function createCircles() {
-    var circle1 = new Circle(100, 100, 0.5, 5, "black", 0, 0, 0);
-    circles.push(circle1);
-    circles[0].draw(ctx);
+    circles = [];
 
-    var circle2 = new Circle(150, 150, 1, 20, "red", 4, 2, 1);
-    circles.push(circle2);
-    circles[1].draw(ctx);
+    const numCircles = numCirclesSlider.value;
 
-    var circle3 = new Circle(75, 125, 0.75, 10, "green", 1, 1, 2);
-    circles.push(circle3);
-    circles[2].draw(ctx);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    startButton.onclick = function updateCircles() {
-        requestAnimationFrame(updateCircles);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(var n = 0; n < numCircles; n++) {
+        var radius = Math.floor(Math.random() * 26) + 5; 
+        var mass = Math.pow(radius, 2) / 100;
+        var x = Math.random() * (canvas.width - radius) + radius;
+        var y = Math.random() * (canvas.height - radius) + radius;
+        var color = "black";
+        var dx = (Math.random() * 4 + 0.5);
+        var dy = (Math.random() * 4 + 0.5);
 
-        for(var i = 0; i < circles.length; i++) {
-            circles[i].updatePosition();
-        }
+        circles[n] = (new Circle(x, y, mass, radius, color, dx, dy, n));
+        circles[n].draw(ctx);
 
-        ctx.fillText(circle1.xvel.toFixed(2), 20, 20);
-        ctx.fillText(circle1.yvel.toFixed(2), 20, 30);
-        ctx.fillText(circle2.xvel.toFixed(2), 50, 20);
-        ctx.fillText(circle2.yvel.toFixed(2), 50, 30);
     }
 }
 
-createCircles();
+function updateCircles() {
+    ctx.fillText(circles.length, 100, 100);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for(var i = 0; i < circles.length; i++) {
+        circles[i].updatePosition();
+    }
+
+    requestAnimationFrame(updateCircles);
+}
+
+numCirclesSlider.onchange = function() {
+    createCircles();
+}
+
+startButton.onclick = function() {
+    updateCircles();
+}
